70d31e1cde8d5647427c294545f89c95059b88e6
DanSimonson <dansimonson@mariposaweb.net>
2024-07-22 15:04:34 +0800

    added new advanced banner

--- src/components/AdvancedBanner.jsx
+++ src/components/AdvancedBanner.jsx
@@ -1,69 +1,20 @@
 import { motion, useMotionValue, useTransform } from "framer-motion";
 
-export const Example = () => {
-  const blockVariants = {
-    initial: {
-      rotate: 0,
-    },
-    target: {
-      rotate: 360,
-    },
-  };
-
-  const rotate = useMotionValue(0);
-  /**
-   * Here we tie together the value of "scale" to the value
-   * of "rotate"
-   * The scale will increase along the rotation, from 0
-   * until the rotation reaches 270 degrees ([0, 270])
-   * where the scale property will be equal to 1 ([0, 1]).
-   * The scale will stop increasing while the rotation
-   * finishes its transition
-   *
-   * You can try to modify the values below, and see how it
-   * impacts the resulting transition.
-   */
-  const scale = useTransform(rotate, [0, 270], [0, 1]);
-
-  return (
-    <motion.div
-      style={{
-        background: "linear-gradient(90deg,#ffa0ae 0%,#aacaef 75%)",
-        height: "100px",
-        width: "100px",
-        borderRadius: "10px",
-        rotate,
-        scale,
-      }}
-      variants={blockVariants}
-      initial="initial"
-      animate="target"
-      transition={{
-        ease: "easeInOut",
-        duration: 4,
-      }}
-    />
-  );
-};
-
 export const AdvancedBanner = () => {
+  let x = window.innerWidth / 2;
   const rotate = useMotionValue(0);
-  const scale = useTransform(rotate, [0, 290], [0, 1]);
-  //const opacityOutput = [0, 0.5, 1];
-  //const xInput = [-600, 3000]
-  //const opacity = useTransform(x, xInput opacityOutput)
+  const scale = useTransform(rotate, [0, 360], [0, 4]);
 
   const AdvancedVariants = {
     wildIn: {
-      x: -600,
-      rotate: -5,
+      x: x,
+      rotate: 0,
       scale: 1,
     },
     wildOut: {
-      rotate: 5,
-      x: 3000,
-      scale: 3,
-      boxShadow: "18px 18px 0 rgba(0, 0, 0, 0.2)",
+      rotate: 360,
+      x: x,
+      scale: 4,
     },
   };
 
@@ -76,6 +27,8 @@ export const AdvancedBanner = () => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        rotate,
+        scale,
       }}
       variants={AdvancedVariants}
       initial="wildIn"
@@ -84,10 +37,8 @@ export const AdvancedBanner = () => {
         type: "tween",
         ease: "easeInOut",
         repeat: Infinity,
-        repeatType: "loop",
-        repeatDelay: 3,
-        duration: 12,
-        delay: 2,
+        repeatType: "mirror",
+        duration: 6,
       }}
     >
       Read My Blog for more cool stuff
